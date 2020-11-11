85bc3dc6b93cd556fb3bb85d4392367182d563ec
christian quispe <christianquispecamasca@gmail.com>
2020-11-10 22:46:18 -0500

    chore: add code to test

---
+++ scr/index.js
@@ -0,0 +1,17 @@
+const cities = ["mexico", "lima", "bogota", "buenos aires"];
+
+const randomString = () => {
+  const string = cities[Math.floor(Math.random() * cities.length)];
+  return string;
+};
+
+const reverseString2 = (str) => {
+  return new Promise(( resolve, reject ) =>  {
+    if(!str) {
+      reject(Error('Error'))
+    }
+    resolve(str.split("").reverse().join(''))
+  })
+}
+
+module.exports = randomString;
